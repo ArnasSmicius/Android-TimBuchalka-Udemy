2e6aa4cd555b291f0dc0e4413ed525c27f5cec7f
ArnasSmicius <arnasmic@gmail.com>
2017-10-07 16:28:52 +0300

    Improved ListViews, adapters and custom adapters

--- Top10Downloader/app/src/main/java/com/example/arnas/top10downloader/MainActivity.java
+++ Top10Downloader/app/src/main/java/com/example/arnas/top10downloader/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: starting Asynctask");
         DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml");
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
         Log.d(TAG, "onCreate: done");
     }
 
