bc6ec2b614274a8ac059366b08ad021d67fd6c94
ArnasSmicius <arnasmic@gmail.com>
2017-11-11 08:22:06 +0200

    Implementing RecyclerViewAdapter methods and importing Picasso library

--- FlickrBrowser/app/src/main/java/com/example/arnassmicius/flickrbrowser/FlickrRecyclerViewAdapter.java
+++ FlickrBrowser/app/src/main/java/com/example/arnassmicius/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -3,7 +3,9 @@ package com.example.arnassmicius.flickrbrowser;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +23,33 @@ class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewA
     public FlickrRecyclerViewAdapter(Context context, List<Photo> photosList) {
         mPhotosList = photosList;
         mContext = context;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhoto) {
+        mPhotosList = newPhoto;
+        notifyDataSetChanged();
+    }
+
+    public Photo getOhoto(int position) {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
     }
 
     static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
