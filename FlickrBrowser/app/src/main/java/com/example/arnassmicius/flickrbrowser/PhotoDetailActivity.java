e8fc3f68d72f5d2be9062c84263df7c19e9a5a2e
ArnasSmicius <arnasmic@gmail.com>
2017-12-04 20:08:07 +0200

    Creating functionality in PhotoDetailActivity

--- FlickrBrowser/app/src/main/java/com/example/arnassmicius/flickrbrowser/PhotoDetailActivity.java
+++ FlickrBrowser/app/src/main/java/com/example/arnassmicius/flickrbrowser/PhotoDetailActivity.java
@@ -1,6 +1,11 @@
 package com.example.arnassmicius.flickrbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoDetailActivity extends BaseActivity {
 
@@ -10,5 +15,24 @@ public class PhotoDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_photo_detail);
 
         activateToolbar(true);
+
+        Intent intent = getIntent();
+        Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        if(photo != null) {
+            TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+            photoTitle.setText("Title: " + photo.getTitle());
+
+            TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+            photoTags.setText("Tags: " + photo.getTags());
+
+            TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
+            photoAuthor.setText(photo.getAuthor());
+
+            ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
+            Picasso.with(this).load(photo.getLink())
+                    .error(R.drawable.placeholder)
+                    .placeholder(R.drawable.placeholder)
+                    .into(photoImage);
+        }
     }
 }
